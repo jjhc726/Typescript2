a720b001a59e5376b2cbff447cff2036e9a4ca93
jjhc726 <jj.hurtadoc1@uniandes.edu.co>
2024-10-20 22:54:22 -0500

    info serie

--- Typescript/Serie.js
+++ Typescript/Serie.js
@@ -1,15 +1,11 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Serie = void 0;
-class Serie {
-    constructor(numero, nombre, canal, temporadas, descripcion, pagina, imagen) {
+export class Serie {
+    constructor(numero, nombre, canal, temporada, descripcion, pagina, imagen) {
         this.numero = numero;
         this.nombre = nombre;
         this.canal = canal;
-        this.temporada = temporadas;
+        this.temporada = temporada;
         this.descripcion = descripcion;
         this.pagina = pagina;
         this.imagen = imagen;
     }
 }
-exports.Serie = Serie;
--- Typescript/main.js
+++ Typescript/main.js
@@ -1,34 +1,48 @@
-import {data} from './data.js';
- var seriesTabla = document.getElementById('series');
- var temporadasPromedio =  document.getElementById('promedio');
- renderSeriesInTable(data);
+import { series } from './data.js';
 
+var seriesTabla = document.getElementById('series');
+var temporadasPromedio = document.getElementById('promedio');
+var seriesDetail = document.getElementById('series-detail');
 
- temporadasPromedio.innerHTML = "Promedio temporada es: ".concat(getTemporadasProm(data));
+renderSeriesInTable(series);
+temporadasPromedio.innerHTML = "Seasons average: " + getTemporadasProm(series);
 
-function renderSeriesInTable(series){
-    series.forEach(function (s){
-        var Element = document.createElement("tr");
-        Element.innerHTML = "<td>".concat(s.id, "</td>\n <td style=\"color: #4895D9;\" >").concat(s.nombre, "</td>\n <td>").concat(s.canal, "</td>\n <td>").concat(s.temporada, "</td>");
+function renderSeriesInTable(series) {
+    series.forEach(function (s) {
+        var Element = document.createElement('tr');
+        Element.innerHTML = `
+            <td>${s.numero}</td>
+            <td style="color: #4895D9; cursor: pointer;">${s.nombre}</td>
+            <td>${s.canal}</td>
+            <td>${s.temporada}</td>
+        `;
+
+        Element.addEventListener('click', function () {
+            showSeriesDetail(s);
+        });
+
         seriesTabla.appendChild(Element);
     });
-    
 }
 
-function mostrarPromedioTemporadas(series) {
-    var promedio = getTemporadasProm(series);
-    var Element = document.createElement("tr");
-    Element.innerHTML = "<td colspan='3'>Promedio de temporadas: </td><td>".concat(promedio, "</td>");
-    seriesTabla.appendChild(Element);
+function showSeriesDetail(serie) {
+    seriesDetail.innerHTML = `
+        <div class="card" style="width: 18rem;">
+            <img src="${serie.imagen}" class="card-img-top" alt="${serie.nombre}">
+            <div class="card-body">
+                <h5 class="card-title">${serie.nombre}</h5>
+                <p class="card-text">${serie.descripcion}</p>
+                <a href="${serie.pagina}" class="btn btn-primary" target="_blank">Go to page</a>
+            </div>
+        </div>
+    `;
 }
 
 function getTemporadasProm(series) {
-    var totalTemporadas =0;
-    var nSeries =0;
-    series.forEach(function (s){
-       totalTemporadas += s.temporada;
-       nSeries+=1; 
-    })
-    return totalTemporadas/nSeries;
+    var totalTemporadas = 0;
+    var nSeries = series.length;
+    series.forEach(function (s) {
+        totalTemporadas += s.temporada;
+    });
+    return (totalTemporadas / nSeries).toFixed(0);
 }
-
